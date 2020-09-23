e9a46249d9391e2f4da351c14e7e1a2ba8453520
Johan Pereira <2079700+jwpereira@users.noreply.github.com>
2020-09-23 17:31:55 -0400

    Start tests

---
+++ src/scriptgen.unit.test.ts
@@ -0,0 +1,14 @@
+import * as scriptgen from "./scriptgen";
+
+describe("command generation", () => {
+    it("works with blank input", () => {
+        const options: scriptgen.RunTestsOptions = {
+            JUnitTestResults: "",
+            CoberturaCodeCoverage: "",
+            SourceFolder: "",
+        };
+
+        const actual = scriptgen.generateCommand(options);
+        expect(actual).toBeDefined();
+    });
+});
