1e079ec90066b49667a4d0732de24e6ca91a6f64
David Buzinski <103441853+davidbuzinski@users.noreply.github.com>
2022-08-12 13:57:23 -0400

    Use new run-matlab-command (#25)
    * update to use new run-matlab-command binary

--- scripts/setupdeps.sh
+++ scripts/setupdeps.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
-RMC_URL='https://ssd.mathworks.com/supportfiles/ci/run-matlab-command/v0/run-matlab-command.zip'
 SCRIPTGEN_URL='https://ssd.mathworks.com/supportfiles/ci/matlab-script-generator/v0/matlab-script-generator.zip'
+RMC_BASE_URL='https://ssd.mathworks.com/supportfiles/ci/run-matlab-command/v1'
+SUPPORTED_OS=('win64' 'maci64' 'glnxa64')
 
 # Create dist directory if it doesn't already exist
 DISTDIR="$(pwd)/dist"
-mkdir -p $DISTDIR
+mkdir -p $DISTDIR/bin
 
 # Download and extract in a temporary directory
-WORKINGDIR=$(mktemp -d -t rmc_build)
+WORKINGDIR=$(mktemp -d -t rmc_build.XXXXXX)
 cd $WORKINGDIR
 
-# Download Run MATLAB Command helpers
-wget -O bin.zip $RMC_URL 
-unzip -qod bin bin.zip
-mv -f bin $DISTDIR/
+wget -O  "$WORKINGDIR/license.txt" "$RMC_BASE_URL/license.txt"
+wget -O  "$WORKINGDIR/thirdpartylicenses.txt" "$RMC_BASE_URL/thirdpartylicenses.txt"
 
-# Download Scriptgen helpers
+for os in ${SUPPORTED_OS[@]}
+do
+    if [[ $os == 'win64' ]] ; then
+        bin_ext='.exe'
+    else
+        bin_ext=''
+    fi
+    mkdir -p "$WORKINGDIR/$os"
+    wget -O  "$WORKINGDIR/$os/run-matlab-command$bin_ext" "$RMC_BASE_URL/$os/run-matlab-command$bin_ext"
+done
+
 wget -O scriptgen.zip $SCRIPTGEN_URL
 unzip -qod scriptgen scriptgen.zip
 mv -f scriptgen $DISTDIR
+rm scriptgen.zip
 
+mv -f ./* "$DISTDIR/bin"
 rm -rf $WORKINGDIR
+
