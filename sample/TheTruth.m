1ae1bfcf56a2930f02ec92c3eeb307abb48cbc3e
Kapil Gupta <kapilg@mathworks.com>
2025-07-29 12:31:07 +0530

    introduce test failure

--- sample/TheTruth.m
+++ sample/TheTruth.m
@@ -2,7 +2,7 @@ classdef TheTruth < matlab.unittest.TestCase
     methods (Test)
         function testTheTestOfAllTime(testCase)
             onetyone = 11;
-            testCase.verifyEqual(onetyone, 11);
+            testCase.verifyEqual(onetyone, 12);
         end
     end
 end
