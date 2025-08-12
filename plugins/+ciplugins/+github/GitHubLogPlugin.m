712cd9353df5156ecf029c4f90efd13fb07aaf10
Kapil Gupta <kapilg@mathworks.com>
2025-08-12 11:29:24 +0530

    add test class grouping

--- plugins/+ciplugins/+github/GitHubLogPlugin.m
+++ plugins/+ciplugins/+github/GitHubLogPlugin.m
@@ -2,6 +2,17 @@ classdef GitHubLogPlugin < matlab.unittest.plugins.TestRunnerPlugin
     % Copyright 2025 The MathWorks, Inc.
     
     methods (Access=protected)
+        function runTestClass(plugin, pluginData)
+            % Add GitHub workflow command for starting a test output group
+            disp("::group::" + pluginData.TestSuite.Name + "- class");
+
+            % Invoke the superclass method
+            runTestClass@matlab.unittest.plugins.TestRunnerPlugin(plugin, pluginData);
+
+            % End the test output group
+           disp("::endgroup::");
+        end
+
         function runTest(plugin, pluginData)
             % Add GitHub workflow command for starting a test output group
             disp("::group::" + pluginData.TestSuite.Name);
