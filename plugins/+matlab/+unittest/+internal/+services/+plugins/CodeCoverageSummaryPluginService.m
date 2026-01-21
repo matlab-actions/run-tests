2df64b464b5a1a891b436e01e8c57984ee0ad822
tagupta <tagupta@mathworks.com>
2026-01-21 12:05:05 +0530

    fix source folder

--- plugins/+matlab/+unittest/+internal/+services/+plugins/CodeCoverageSummaryPluginService.m
+++ plugins/+matlab/+unittest/+internal/+services/+plugins/CodeCoverageSummaryPluginService.m
@@ -20,7 +20,7 @@ classdef CodeCoverageSummaryPluginService < matlab.buildtool.internal.services.c
             
                 
                 %addpath("C:\Users\tagupta\Downloads\common-utils\plugins\sourceFolder");
-                sourceFolder = fullfile(pwd, 'plugins', 'sourceFolder');
+                sourceFolder = fullfile(pwd, 'sample');
                 %coveragePlugin = matlab.unittest.plugins.CodeCoveragePlugin.forFolder(...
                 %    "../../../../../sourceFolder", 'Producing', format, 'MetricLevel', 'mcdc');
                 coveragePlugin = matlab.unittest.plugins.CodeCoveragePlugin.forFolder(...
