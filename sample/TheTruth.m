d6423ed94c9bfe1a2592b4b921b9230744d4964b
tagupta <tagupta@mathworks.com>
2026-01-21 12:02:13 +0530

    remove unnecessary tests

--- sample/TheTruth.m
+++
@@ -1,8 +0,0 @@
-classdef TheTruth < matlab.unittest.TestCase
-    methods (Test)
-        function testTheTestOfAllTime(testCase)
-            onetyone = 11;
-            testCase.verifyEqual(onetyone, 11);
-        end
-    end
-end
--- sample/tAssumptionFailure.m
+++
@@ -1,7 +0,0 @@
-classdef tAssumptionFailure < matlab.unittest.TestCase
-    methods (Test)
-        function testAssumptionFailure(testCase)
-            testCase.assumeEqual(1+1, 11);
-        end
-    end
-end
