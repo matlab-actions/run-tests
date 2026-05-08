8e66b3c054e514dbfde26b568fc9b49b974431ca
Kapil Gupta <kapilg@mathworks.com>
2026-05-08 22:42:36 +0530

    make prettier

--- src/index.ts
+++ src/index.ts
@@ -74,7 +74,12 @@ async function run() {
                 const runId = process.env.GITHUB_RUN_ID || "";
                 const actionName = process.env.GITHUB_ACTION || "";
 
-                testResultsSummary.processAndAddTestSummary(runnerTemp, runId, actionName, workspaceDir);
+                testResultsSummary.processAndAddTestSummary(
+                    runnerTemp,
+                    runId,
+                    actionName,
+                    workspaceDir,
+                );
                 core.summary.write();
             }
         });
